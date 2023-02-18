145c22eca7af36e67397618722ab4d557a208d52
aldairdiaz <irving.s21diaz@gmail.com>
2023-02-17 21:07:35 -0600

    "Initial Commit"

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // Back-end Path Portfolio project: Mixed Messages 15-feb-23
 'use strict';
 
+// Topic: motivational quotes
+
+//  1: fill up the data
+const data = {
+    greet: [""]
+
+}
+
+//  2: create the message
+
+//  3: display the message
